a5b544903b95733aa854c3bcb0a202ad2e7adc30
B-Interactive <16984998+B-Interactive@users.noreply.github.com>
2025-08-04 15:09:41 +1000

    Return the mistakenly removed makeDMXPacket() function.

--- src/binteractive/artnetsocket/ArtNetHelper.hx
+++ src/binteractive/artnetsocket/ArtNetHelper.hx
@@ -2,6 +2,7 @@ package binteractive.artnetsocket;
 
 import openfl.utils.ByteArray;
 import binteractive.artnetsocket.ArtNetTypes;
+import haxe.ds.StringMap;
 
 /**
  * Art-Net protocol helper for DMX, Poll, and PollReply packets.
@@ -104,6 +105,83 @@ class ArtNetHelper {
         ba.writeByte(0); // Priority
         ba.position = 0;
         return ba;
+    }
+
+    /**
+     * Creates an ArtDMXPacket from various DMX data representations.
+     *
+     * Usage examples:
+     *   - makeDMXPacket([0, 255, 128, ...])
+     *   - makeDMXPacket({universe: 0, values: [...]})
+     *   - makeDMXPacket(map) // where map is a StringMap with keys "universe", "length", "data"
+     *
+     * @param input Array<Int>, object, or StringMap<String,Dynamic> describing DMX packet data.
+     * @return ArtDMXPacket
+     */
+    public static function makeDMXPacket(input:Dynamic):ArtDMXPacket {
+        var universe:Int = 0;
+        var dmxData:ByteArray = new ByteArray();
+        var length:Int = 512;
+
+        // If input is Array<Int>
+        if (Std.is(input, Array)) {
+            var arr:Array<Int> = cast input;
+            length = arr.length;
+            for (v in arr) dmxData.writeByte(v);
+        }
+        // If input is an object with fields
+        else if (Reflect.isObject(input) && !Std.is(input, StringMap)) {
+            if (Reflect.hasField(input, "universe")) universe = Reflect.field(input, "universe");
+            if (Reflect.hasField(input, "values")) {
+                var vals:Array<Int> = Reflect.field(input, "values");
+                length = vals.length;
+                for (v in vals) dmxData.writeByte(v);
+            }
+            if (Reflect.hasField(input, "length")) length = Reflect.field(input, "length");
+            if (Reflect.hasField(input, "data")) {
+                dmxData = Reflect.field(input, "data");
+                if (Reflect.hasField(input, "length")) length = Reflect.field(input, "length");
+                else length = dmxData.length;
+            }
+        }
+        // If input is a StringMap<String,Dynamic>
+        else if (Std.is(input, StringMap)) {
+            var map:StringMap<Dynamic> = cast input;
+            if (map.exists("universe")) universe = map.get("universe");
+            if (map.exists("length")) length = map.get("length");
+            if (map.exists("data")) dmxData = map.get("data");
+            else if (map.exists("values")) {
+                var vals:Array<Int> = map.get("values");
+                length = vals.length;
+                for (v in vals) dmxData.writeByte(v);
+            }
+        }
+        else throw "Invalid argument for makeDMXPacket";
+
+        // Ensure DMX data is correct length
+        dmxData.position = 0;
+        if (dmxData.length < length) {
+            while (dmxData.length < length) dmxData.writeByte(0);
+        } else if (dmxData.length > length) {
+            dmxData = dmxData.sub(0, length);
+        }
+
+        // ArtDMXPacket structure must be matched to your ArtNetTypes definition.
+        // This sample assumes a constructor or static create method like:
+        // ArtDMXPacket.create(universe, length, dmxData)
+        // If your structure is a plain object, adjust accordingly.
+        #if (haxe_ver >= "4.0")
+        return ArtDMXPacket.create(universe, length, dmxData);
+        #else
+        return {
+            protocolVersion: 14,
+            sequence: 0,
+            physical: 0,
+            universe: universe,
+            length: length,
+            data: dmxData
+        };
+        #end
     }
 
     // Add decodePollReply if needed for your use case!
