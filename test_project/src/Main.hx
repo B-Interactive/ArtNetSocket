91f52f60fb1aaaea552ccd3c5cea96722e7b770f
B-Interactive <16984998+B-Interactive@users.noreply.github.com>
2025-08-05 09:37:31 +1000

    Integrate basic tests.

---
+++ test_project/src/Main.hx
@@ -0,0 +1,9 @@
+import binteractive.artnetsocket.ArtNetSocket;
+import binteractive.artnetsocket.ArtNetTypes;
+import binteractive.artnetsocket.ArtNetSocketEvents;
+
+class Main {
+    static function main() {
+        var socket = new ArtNetSocket();
+    }
+}
